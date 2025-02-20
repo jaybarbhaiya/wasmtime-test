cc2253df4ddeef8f5eb10388f347e96b18857c47
Jay Barbhaiya <jay.barbhaiya@sap.com>
2025-02-20 15:23:21 +0100

    refactor

--- runner/benches/wasm_module.rs
+++ runner/benches/wasm_module.rs
@@ -1,53 +1,25 @@
-use std::env::current_dir;
-
 use criterion::{black_box, criterion_group, criterion_main};
-use lazy_static::lazy_static;
 use wasmtime::{Engine, Module};
 
-lazy_static! {
-    static ref WASM_BYTES: Vec<u8> = read_wasm_bytes();
-}
+fn create_module() -> Module {
+    let engine = Engine::default();
+    Module::new(&engine, include_bytes!("../../test.wasm")).unwrap()
 
-fn read_wasm_bytes() -> Vec<u8> {
-    let wasm_path = current_dir()
-        .expect("cannot get current directory")
-        .join("..")
-        .join("target")
-        .join("wasm32-unknown-unknown")
-        .join("release")
-        .join("wasmtime_test.wasm");
-    std::fs::read(&wasm_path).unwrap()
-}
-
-pub struct WasmBuilder<'a> {
-    wasm_bytes: &'a [u8],
-}
-
-impl<'a> WasmBuilder<'a> {
-    pub fn new(wasm_bytes: &'a [u8]) -> Self {
-        let wasm_bytes = wasm_bytes;
-        Self { wasm_bytes }
-    }
-
-    pub fn build(&self) -> Result<Module, wasmtime::Error> {
-        let engine = Engine::default();
-        Module::new(&engine, &self.wasm_bytes)
-    }
 }
 
 fn wasm_module(c: &mut criterion::Criterion) {
     let mut group = c.benchmark_group("wasm_module");
     group.bench_function("creation", |b| {
-        b.iter(|| {
-            let _ = black_box(WasmBuilder::new(&WASM_BYTES).build().unwrap());
+        b.iter_with_large_drop(|| {
+            black_box(create_module());
         })
     });
 
     group.bench_function("destruction", |b| {
         b.iter_batched(
-            || WasmBuilder::new(&WASM_BYTES).build().unwrap(),
+            || create_module(),
             |module| {
-                black_box(drop(module));
+                drop(black_box(module));
             },
             criterion::BatchSize::SmallInput,
         );
